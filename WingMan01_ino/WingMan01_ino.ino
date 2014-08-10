6a74c5f685c54bb2fe2156e23387982e8939bb8d
Alexander Thomas <me@alexander-thomas.net>
2014-08-10 11:44:30 +0200

    cleaned up some code and made it work with an uno board and an ethernet shield

--- WingMan01_ino/WingMan01_ino.ino
+++ WingMan01_ino/WingMan01_ino.ino
@@ -10,26 +10,35 @@
 #include <Ethernet.h>
 #include <EthernetClient.h>
 
-// This example downloads the URL "http://arduino.cc/"
+// Colour export pin decleration
+int redPin = 9;
+int greenPin = 10;
+int bluePin = 3; 
 
 // Name of the server we want to connect to
-const char kHostname[] = "mywingman.alexander-thomas.net ";
+const char kHostname[] = "mywingman.alexander-thomas.net";
 // Path to download (this is the bit after the hostname in the URL
 // that you want to download
 const char kPath[] = "/user/6b8189b581066829239fecb4f3190e89354e29f0e989b021c0292c8fe987fee5/color";
 
-byte mac[] = { 0xDE, 0xB8, 0xF5, 0x71, 0xA8, 0xC1 };
+byte mac[] = { 
+  0xDE, 0xB8, 0xF5, 0x71, 0xA8, 0xC1 };
 
 // Number of milliseconds to wait without receiving any data before we give up
 const int kNetworkTimeout = 30*1000;
 // Number of milliseconds to wait if no data is available before trying again
 const int kNetworkDelay = 1000;
 
+int colors[3] = {0, 0, 0};  
+
 void setup()
 {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600); 
-
+  
+  //Initialize the lamp with 0,0,0
+  writeColorsToLamp(colors);
+  
   while (Ethernet.begin(mac) != 1)
   {
     Serial.println("Error getting IP address via DHCP, trying again...");
@@ -38,121 +47,127 @@ void setup()
 }
 
 void loop()
-{
-    Serial.println("Loop");
-    String colors;
-    // String color1;
-    // String color2;
-    // String color3;
-    
-    // if(magicBluetoothEvent == true)
-    // {
-        colors = fetchColorCode();
-        logMsg(colors);
-    //     color1 = color.substr(1,3);
-    //     color2 = color.substr(5,8);
-    //     color3 = color.substr(10,13);
-    //
-    //     //Sende Farben an die Lampe
-    //
-    //
-    //     //Warte 90s
-    //     delay(90);
-    //
-    //     if(magicBluetoothEvent == false){
-    //         //Schalte Lampe aus
-    //     }else{
-    //         delay(90);
-    //     }
-    // }
+{  
+  String colorsString;
 
-delay(2000);
-    // And just stop, now that we've tried a download
-    //while(1);
+  //Fetch colors from the remote api
+  colorsString = fetchColorsFromRemote();
+  //Process return valuer from remote
+  processColorsString(colorsString, colors);
+  //Write color valuzes to the lamp
+  writeColorsToLamp(colors);
+
+  // And just stop, now that we've tried a download
+  while(1);
 }
 
-String fetchColorCode(){
-    int err = 0;
-  
-    EthernetClient c;
-    HttpClient http(c);
-    String response;
-  
-    err = http.get(kHostname, kPath);
-    if (err == 0)
-    {
-      logMsg("startedRequest ok");
+void writeColorsToLamp(int colors[]){
+  Serial.println("writeColorsToLamp()");
 
-      err = http.responseStatusCode();
+  Serial.println("Setting red to " + String(colors[0]));
+  Serial.println("Setting green to " + String(colors[1]));
+  Serial.println("Setting blue to " + String(colors[2]));
+
+  analogWrite(redPin, colors[0]);
+  analogWrite(greenPin, colors[1]);
+  analogWrite(bluePin, colors[2]);
+}
+
+void processColorsString(String colorsString, int colors[]){
+  Serial.println("processColorsString()");
+
+  String redStr;
+  String greenStr;
+  String blueStr;
+
+  redStr = colorsString.substring(3,6);
+  colors[0] = redStr.toInt();
+
+  greenStr = colorsString.substring(7,10);
+  colors[1] = greenStr.toInt();
+
+  blueStr = colorsString.substring(11,14);
+  colors[2] = blueStr.toInt();  
+}
+
+//Fetches color code from the wingman api
+String fetchColorsFromRemote(){
+  Serial.println("fetchColorsFromRemote()");
+
+  String response;
+  int err = 0;
+  EthernetClient c;
+  HttpClient http(c);
+
+  err = http.get(kHostname, kPath);
+  if (err == 0)
+  {
+    Serial.println("startedRequest ok");
+
+    err = http.responseStatusCode();
+    if (err >= 0)
+    {
+      Serial.print("Got status code: ");
+      Serial.println(err);
+
+      // Usually you'd check that the response code is 200 or a
+      // similar "success" code (200-299) before carrying on,
+      // but we'll print out whatever response we get
+
+      err = http.skipResponseHeaders();
       if (err >= 0)
       {
-        logMsg("Got status code: ");
-        logMsg(String(err));
+        int bodyLen = http.contentLength();
+        Serial.print("Content length is: ");
+        Serial.println(bodyLen);
+        Serial.println();
+        Serial.println("Body returned follows:");
 
-        // Usually you'd check that the response code is 200 or a
-        // similar "success" code (200-299) before carrying on,
-        // but we'll print out whatever response we get
-        err = http.skipResponseHeaders();
-        if (err >= 0)
+        // Now we've got to the body, so we can print it out
+        unsigned long timeoutStart = millis();
+        char c;
+        // Whilst we haven't timed out & haven't reached the end of the body
+        while ( (http.connected() || http.available()) &&
+          ((millis() - timeoutStart) < kNetworkTimeout) )
         {
-          int bodyLen = http.contentLength();
-          logMsg("Content length is: ");
-          logMsg(String(bodyLen));
-          logMsg("");
-      
-          // Now we've got to the body, so we can print it out
-          unsigned long timeoutStart = millis();
-          char c;
-          // Whilst we haven't timed out & haven't reached the end of the body
-          while ( (http.connected() || http.available()) &&
-                 ((millis() - timeoutStart) < kNetworkTimeout) )
+          if (http.available())
           {
-              if (http.available())
-              {
-                  c = http.read();
-                  response += c;
-                
-                  // Print out this character
-                  logMsg(String(c));
-               
-                  bodyLen--;
-                  // We read something, reset the timeout counter
-                  timeoutStart = millis();
-              }
-              else
-              {
-                  // We haven't got any data, so let's pause to allow some to
-                  // arrive
-                  delay(kNetworkDelay);
-              }
+            c = http.read();
+            // Print out this character
+            Serial.print(c);
+            response += c;
+
+            bodyLen--;
+            // We read something, reset the timeout counter
+            timeoutStart = millis();
           }
-        }
-        else
-        {
-          logMsg("Failed to skip response headers: ");
-          logMsg(String(err));
+          else
+          {
+            // We haven't got any data, so let's pause to allow some to
+            // arrive
+            delay(kNetworkDelay);
+          }
         }
       }
       else
-      {    
-        logMsg("Getting response failed: ");
-        logMsg(String(err));
+      {
+        Serial.print("Failed to skip response headers: ");
+        Serial.println(err);
       }
     }
     else
-    {
-      logMsg("Connect failed: ");
-      logMsg(String(err));
+    {    
+      Serial.print("Getting response failed: ");
+      Serial.println(err);
     }
-    http.stop();
-    
-    return response;
+  }
+  else
+  {
+    Serial.print("Connect failed: ");
+    Serial.println(err);
+  }
+  http.stop();
+  return response;
 }
 
-void logMsg(String msg){
-    bool debug = true;
-    if(debug = true)
-    {
-       Serial.println(msg); 
-    }
-}
+
